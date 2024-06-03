0d78973c8ca5031bf9058a78767380347b8fdaf7
Kakao-max <angrybird4837@gmail.com>
2024-06-03 11:15:11 -0400

    updated

--- tools/sifgen/node/types/genesis.go
+++ tools/sifgen/node/types/genesis.go
@@ -8,7 +8,15 @@ type Value struct {
 	Memo string `json:"memo"`
 }
 
+type Gentxs []Gentx
+type Gentx struct {
+	Type  string `json:"type"`
+	Value Value  `json:"value"`
+}
 
+type Genutil struct {
+	Gentxs Gentxs `json:"gentxs"`
+}
 
 
 
